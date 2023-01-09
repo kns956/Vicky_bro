80ed84782f55481e4e08fbe5825fd0c141dbd428
kns956 <knsathyabe956@gmai.com>
2023-01-09 22:15:40 +0530

    sathya

---
+++ src/test/java/com/test/Sathya.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import org.junit.Test;
+
+public class Sathya {
+
+	@Test
+	public void tc01() {
+		System.out.println("1st pass");
+	}
+
+	@Test
+	public void tc02() {
+		System.out.println("2nd pass");
+	}
+
+	@Test
+	public void tc03() {
+		System.out.println("3rd pass");
+	}
+
+	@Test
+	public void tc04() {
+		System.out.println("4th pass");
+	}
+
+	@Test
+	public void tc05() {
+		System.out.println("5th pass");
+	}
+
+}
